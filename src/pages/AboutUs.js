373b6162bda64cbc6541f91f1841f0e17d4d6671
kafleBeast <abhishek.kafle@stonybrook.edu>
2021-07-13 00:05:09 +0900

    Updates

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function AboutUs() {
     return (
-        <div>
-            ABOUT US
+        <div className="about-us">
+            <h1 className="aboutUS-title"> ABOUT US </h1>
+            <div className="aboutUs-contents">
+                <div className="imageAbhishek aboutUsImage">Abhishek Image here</div>
+                <div className="imageCrispen aboutUsImage">Crispen Image here</div>
+                <div className="imageNilson aboutUsImage">Nilson Image here</div>
+            </div>
+            <div className="aboutUs-contents">
+                <div className="contentAbhishek aboutUsContent"><p>Some random ipsum lorem here Abhishek</p></div>
+                <div className="contentCrispen aboutUsContent"><p>Some random ipsum lorem here Crispen</p></div>
+                <div className="contentNilson aboutUsContent"><p>Some random ipsum lorem here Nilson</p></div>
+            </div>
         </div>
     )
 }
